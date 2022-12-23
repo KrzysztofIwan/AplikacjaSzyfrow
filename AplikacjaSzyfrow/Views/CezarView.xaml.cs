a8c0c13f48b7faf315f65ce670f7d00d60090796
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-12-23 15:10:01 +0100

    Dodanie szyfru cezara w kodzie oraz nazwanie textbox-ów

--- AplikacjaSzyfrow/Views/CezarView.xaml.cs
+++ AplikacjaSzyfrow/Views/CezarView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Web;
 
 namespace AplikacjaSzyfrow.Views
 {
@@ -27,12 +28,42 @@ namespace AplikacjaSzyfrow.Views
 
         public void Szyfrowanie(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Szyfrowanie");
+            string text = this.JawnyTEXT.Text.ToString();
+            int przesuniecie = 2;
+            if (!String.IsNullOrEmpty(text))
+            {
+                StringBuilder zaszyforwane = WykonajSzyfrowanie(text, przesuniecie);
+                this.ZaszyfrowanyTEXT.Text = zaszyforwane.ToString();
+            }
+
         }
 
         public void Odszyfruj(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Odszyfrowywanie");
+        }
+
+
+        public static StringBuilder WykonajSzyfrowanie(String text, int s)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                {
+                    char ch = (char)(((int)text[i] +
+                                    s - 65) % 26 + 65);
+                    result.Append(ch);
+                }
+                else
+                {
+                    char ch = (char)(((int)text[i] +
+                                    s - 97) % 26 + 97);
+                    result.Append(ch);
+                }
+            }
+            return result;
         }
     }
 }
