701966477293e90baa9393e984752719cd6eea90
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-12-27 12:15:35 +0100

    Dodanie kodu Vigenere'a

--- AplikacjaSzyfrow/Views/VigenereView.xaml.cs
+++ AplikacjaSzyfrow/Views/VigenereView.xaml.cs
@@ -27,12 +27,70 @@ namespace AplikacjaSzyfrow.Views
 
         public void Szyfrowanie(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Szyfrowanie");
+            if (!String.IsNullOrEmpty(this.JawnyTEXT.Text))
+            {
+                this.ZaszyfrowanyTEXT.Text = Decipher(this.JawnyTEXT.Text, "cipher");
+            }
         }
 
-        public void Odszyfruj(object sender, RoutedEventArgs e)
+        public void Odszyfruj(object sender, RoutedEventArgs e) 
         {
-            MessageBox.Show("Odszyfrowywanie");
+            if (!String.IsNullOrEmpty(this.ZaszyfrowanyTEXT.Text))
+            {
+                this.JawnyTEXT.Text = Encipher(this.ZaszyfrowanyTEXT.Text, "cipher");
+            }
+        }
+
+        public void Clear(object sender, RoutedEventArgs e)
+        {
+            this.ZaszyfrowanyTEXT.Text = "";
+            this.JawnyTEXT.Text = "";
+        }
+
+        private static int Mod(int a, int b)
+        {
+            return (a % b + b) % b;
+        }
+
+        private static string Cipher(string input, string key, bool encipher)
+        {
+            for (int i = 0; i < key.Length; ++i)
+                if (!char.IsLetter(key[i]))
+                    return null; // Error
+
+            string output = string.Empty;
+            int nonAlphaCharCount = 0;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    bool cIsUpper = char.IsUpper(input[i]);
+                    char offset = cIsUpper ? 'A' : 'a';
+                    int keyIndex = (i - nonAlphaCharCount) % key.Length;
+                    int k = (cIsUpper ? char.ToUpper(key[keyIndex]) : char.ToLower(key[keyIndex])) - offset;
+                    k = encipher ? k : -k;
+                    char ch = (char)((Mod(((input[i] + k) - offset), 26)) + offset);
+                    output += ch;
+                }
+                else
+                {
+                    output += input[i];
+                    ++nonAlphaCharCount;
+                }
+            }
+
+            return output;
+        }
+
+        public static string Encipher(string input, string key)
+        {
+            return Cipher(input, key, true);
+        }
+
+        public static string Decipher(string input, string key)
+        {
+            return Cipher(input, key, false);
         }
     }
 }
